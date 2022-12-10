10fb2e52c5aeed3cda4128a6c4535114ee753908
Markus Pfeifenberger <pfeifenberger.markus2003@gmail.com>
2022-12-10 13:51:46 +0100

    day10: clippy + fmt

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,7 +1,6 @@
-
 fn main() {
     let content = std::fs::read_to_string("./input.txt").unwrap();
-    
+
     let mut signal: Vec<isize> = Vec::new();
     let mut x = 1;
 
@@ -24,7 +23,15 @@ fn main() {
         }
     }
 
-    println!("solution 1: {}", 20 * signal[20] + 60 * signal[60] + 100 * signal[100] + 140 * signal[140] + 180 * signal[180] + 220 * signal[220]);
+    println!(
+        "solution 1: {}",
+        20 * signal[20]
+            + 60 * signal[60]
+            + 100 * signal[100]
+            + 140 * signal[140]
+            + 180 * signal[180]
+            + 220 * signal[220]
+    );
 
     // part 2
 
@@ -32,7 +39,6 @@ fn main() {
         for x in 0..40 {
             let cycle = y * 40 + x;
             let offset = cycle % 40;
-
 
             if (signal[cycle + 1] as i32).abs_diff(offset as i32) <= 1 {
                 print!("#");
